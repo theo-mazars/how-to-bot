a01c6eb311700d6fbf7fd979d0eaa9eab51018b4
Theo Mazars <theo.mazars@epitech.eu>
2020-10-13 23:56:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Wikihow Bot
+
+In this repo you will find a complete Discord bot that is able to answer to your questions by searching an answer on [wikihow](https://www.wikihow.com/).
+
+## On the road
+
+Follow the project advancement on [Trello](https://trello.com/b/TclNzJlw/how-to-bot)
+
+## Credits
+
+- [Theo Mazars](https://github.com/theo-mazars) (author)
+
+## License
+
+### MIT License
+
+[![License](https://img.shields.io/github/license/theo-mazars/how-to-bot)](https://opensource.org/licenses/MIT)
---
+++ app.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import { readdirSync } from "fs";
+import Discord from "discord.js";
+import { config } from "./src/config.js";
+
+dotenv.config();
+
+const { prefix } = config;
+const Bot = new Discord.Client({ autoReconnect: true });
+const commandFiles = readdirSync("./src/commands").filter((file) =>
+  file.endsWith(".cmd.js")
+);
+
+Bot.commands = new Discord.Collection();
+commandFiles.map(async (file) => {
+  const command = await import(`./src/commands/${file}`);
+  Bot.commands.set(command.name, command);
+});
+
+Bot.on("ready", () => {
+  console.log(`Logged in as ${Bot.user.tag}`);
+});
+
+Bot.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLocaleLowerCase();
+
+  try {
+    Bot.commands.get(command).execute(message, args);
+  } catch (err) {
+    message.reply("Hmmm... 🤔 I think you typed a wrong command...");
+  }
+});
+
+Bot.login(process.env.TOKEN);
